35da0039dbd9d10e23f5fab939fc01a9a9009bb8
Trevor Lee <49603972+trevortylerlee@users.noreply.github.com>
2024-06-07 01:01:02 -0700

    add vercel analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,10 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import vercel from "@astrojs/vercel/static";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  output: "static",
+  adapter: vercel({
+    webAnalytics: { enabled: true },
+  }),
+});
