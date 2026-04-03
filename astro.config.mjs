738c97181641f25e5b32989e2a21fd556540ea51
Trevor Lee <49603972+trevortylerlee@users.noreply.github.com>
2026-04-03 14:18:42 -0700

    cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,6 @@
 import { defineConfig } from "astro/config";
-import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
   output: "static",
-  adapter: cloudflare(),
 });
